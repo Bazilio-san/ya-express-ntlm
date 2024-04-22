44ea6f171a38007ee44ff0dda393c53f79395d7f
vvmakarov <vvmakarov@corp.finam.ru>
2024-04-22 16:12:32 +0300

    0.0.19

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,43 @@ export {
   IAuthNtlmOptionsMandatory,
   EAuthStrategy,
 } from './interfaces';
+
+export {
+  createLMHashedPasswordV1,
+  createNTHashedPasswordV1,
+  extractNtlmMessageFromAuthenticateHeader,
+  Type2Message,
+  NTLMFlag,
+  createDesEncrypt,
+  NTLMTypeFlags,
+} from './node-ntlm-core/core';
+
+export {
+  createMessageType1,
+  Type1MessageOptions,
+} from './node-ntlm-core/createMessageType1';
+
+export { createMessageType2 } from './node-ntlm-core/createMessageType2';
+
+export {
+  createMessageType3,
+  Type3MessageOptions,
+} from './node-ntlm-core/createMessageType3';
+
+export { parseMessageType2 } from './node-ntlm-core/parseMessageType2';
+
+export {
+  NTLMMessageParsed,
+  NTLMMessageType,
+  NtlmParseOptions,
+  NTLMType1,
+  NTLMType2,
+  NTLMType3,
+} from './ntlm-parser/parser-interfaces';
+
+export { NTLMType1Parser } from './ntlm-parser/NTLMType1Parser';
+export { NTLMType2Parser } from './ntlm-parser/NTLMType2Parser';
+export { NTLMType3Parser } from './ntlm-parser/NTLMType3Parser';
+export { AbstractParser } from './ntlm-parser/AbstractParser';
+
+export { ntlmParse } from './ntlm-parser/index';
